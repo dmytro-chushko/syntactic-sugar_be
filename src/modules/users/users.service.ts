319e67f78a3f6fd0a96d972089a7e7bf5719f850
Деркачев Иван <derkachov_ivan@host-192-168-0-102.sohonet.ua>
2022-11-06 15:11:11 +0200

    try/catch, return types added

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,36 +10,62 @@ export class UsersService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  async createUser(userDto: CreateUserDto) {
-    const user = await this.userRepository.create(userDto);
-    return this.userRepository.save(user);
+  async createUser(userDto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.userRepository.create(userDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  async getAllUser() {
-    const users = await this.userRepository.find();
-    return users;
+  async getAllUser(): Promise<User[]> {
+    try {
+      const users = await this.userRepository.find();
+      return users;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  async getUserById(id: string) {
-    const user = await this.userRepository.findOneBy({ id });
-    return user;
+  async getUserById(id: string): Promise<User> {
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+      return user;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  async updateUser(id: string, userDto: CreateUserDto) {
-    const user = await this.userRepository.update({ id }, { ...userDto });
-    return user;
+  async updateUser(id: string, userDto: CreateUserDto): Promise<User> {
+    try {
+      await this.userRepository.update({ id }, { ...userDto });
+      const user = await this.userRepository.findOneBy({ id });
+      return user;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  async deleteUser(id: string) {
-    const user = await this.userRepository.delete({ id });
-    return user;
+  async deleteUser(id: string): Promise<string> {
+    try {
+      await this.userRepository.delete({ id });
+      return `User ${id} has been deleted`;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  async getUserByEmail(email: string) {
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.email= :userEmail', { userEmail: email })
-      .getOne();
-    return user;
+  async getUserByEmail(email: string): Promise<User> {
+    try {
+      const user = await this.userRepository
+        .createQueryBuilder('user')
+        .where('user.email= :userEmail', { userEmail: email })
+        .getOne();
+      return user;
+    } catch (err) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
