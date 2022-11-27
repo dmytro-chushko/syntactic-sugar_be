a9c913fd321b73cb5d22c046d5ec325e9b21a422
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-11-27 03:11:45 +0200

    fix: fix entity

--- src/database/entities/employer.entity.ts
+++ src/database/entities/employer.entity.ts
@@ -12,44 +12,44 @@ export class Employer {
   @ApiProperty()
   @Column({
     unique: false,
-    nullable: false,
+    default: null,
   })
   fullName: string;
 
   @ApiProperty()
   @Exclude()
   @Column({
-    nullable: false,
+    default: null,
   })
   companyName: string;
 
   @ApiProperty()
   @Column({
-    nullable: false,
+    default: null,
   })
   position: string;
 
   @ApiProperty()
   @Column({
-    nullable: false,
+    default: null,
   })
   phone: string;
 
   @ApiProperty()
   @Column({
-    nullable: false,
+    default: null,
   })
   linkedIn: string;
 
   @ApiProperty()
   @Column({
-    nullable: false,
+    default: null,
   })
   website: string;
 
   @ApiProperty()
   @Column({
-    nullable: false,
+    default: null,
   })
   aboutUs: string;
 
