c293ba750c81a08c6ac0c7659521db8ad28d3d3b
Dmytro Chushko <dmytro.chushko@gmail.com>
2023-01-10 23:48:17 +0200

    add relations to getProposalsByJobId

--- src/modules/proposals/services/proposals.service.ts
+++ src/modules/proposals/services/proposals.service.ts
@@ -48,6 +48,11 @@ export class ProposalsService implements IProposalsService {
         .leftJoinAndSelect('proposal.job', 'job')
         .leftJoinAndSelect('job.employer', 'employer')
         .leftJoinAndSelect('proposal.freelancer', 'freelancer')
+        .leftJoinAndSelect('freelancer.country', 'country')
+        .leftJoinAndSelect('freelancer.category', 'category')
+        .leftJoinAndSelect('freelancer.skills', 'skills')
+        .leftJoinAndSelect('freelancer.education', 'education')
+        .leftJoinAndSelect('freelancer.workHistory', 'workHistory')
         .where('job.id = :id', { id })
         .andWhere('employer.userId = :userId', { userId: user.id })
         .getMany();
